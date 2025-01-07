9bb4497354b5a1631c48bc800121cbd487d583f5
Your Name <you@example.com>
2025-01-07 15:34:11 -0500

    okbr

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+
+
+const appSettings = {
+   databaseURL: "https://reel-app-e941a-default-rtdb.firebaseio.com/"
+}
+const app = initializeApp(appSettings)
+console.log(app)
+
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
